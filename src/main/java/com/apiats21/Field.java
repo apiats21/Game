33e43f8372ee11019cff6b8d0cb9bba85d7e15f9
apiats21 <pi134@hotmail.com>
2021-07-06 14:29:25 +0300

    game

--- src/main/java/com/apiats21/Field.java
+++ src/main/java/com/apiats21/Field.java
@@ -2,13 +2,27 @@ package main.java.com.apiats21;
 
 
 public class Field {
-
-
     public enum Figure {Rock, Scissors, Paper}
 
+    private Figure f1;
+    private Figure f2;
 
+    public Figure getF1() {
+        return f1;
+    }
 
-    public enum Options {Rock, Scissor, Paper}
+    public void setF1(Figure f1) {
+        this.f1 = f1;
+    }
+
+    public Figure getF2() {
+        return f2;
+    }
+
+    public void setF2(Figure f2) {
+        this.f2 = f2;
+    }
+
 
     public void welcomeMess() {
         System.out.println("Welcome to the game!!!");
--- src/main/java/com/apiats21/GameLogic.java
+++ src/main/java/com/apiats21/GameLogic.java
@@ -1,29 +1,10 @@
 package main.java.com.apiats21;
-/*Field: приватные поля и методы для доступа к ним?
-
-GameLogic: создать обьект  класса field?
-две ячейки
-минимум 7 методов
-1. get user input
-2. валидировать код?
-3. get pc input
-4. валидировать код?
-5.
-6.
-7.  счет игры, продолжить игу...
-Как в метод compareMoves передать returns методов userMove() и compMove()
-
-Game runner:
-только обьект класса GameLogic
-метод startGame() и все
- */
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class GameLogic {
-    int userMove;
-    int randNum;
+
     Win winner;
     Field.Figure userMoveF;
     Field.Figure randNumF;
@@ -34,58 +15,33 @@ public class GameLogic {
         fl.welcomeMess();
     }
 
-    /*public void userOpt() {
-        Scanner sc = new Scanner(System.in);
-        this.userMove = sc.nextInt();
-       // Field.Figure userMove = Field.Figure.values()[sc.nextInt()-1];
-
-    }
-
-    public void compMove() {
-        this.randNum = new Random().nextInt(3) + 1;
-    }*/
-
-    //_________________________________
-
     public void setUserMove() {
         Scanner sc = new Scanner(System.in);
         randNumF = Field.Figure.values()[sc.nextInt()-1];
+        fl.setF1(randNumF);
     }
 
     public void setCompMove() {
         int rand = new Random().nextInt(3) + 1;
         userMoveF = Field.Figure.values()[rand-1];
+        fl.setF2(userMoveF);
     }
 
     public void compare() {
-        if (randNumF == userMoveF) winner = Win.Nobody;
-        else if(userMoveF == Field.Figure.Rock && randNumF == Field.Figure.Scissors
-                || userMoveF == Field.Figure.Scissors && randNumF == Field.Figure.Paper
-                || userMoveF == Field.Figure.Paper && randNumF == Field.Figure.Rock)
+        if (fl.getF1() == fl.getF2()) winner = Win.Nobody;
+        else if(fl.getF2() == Field.Figure.Rock && fl.getF1() == Field.Figure.Scissors
+                || fl.getF2() == Field.Figure.Scissors && fl.getF1() == Field.Figure.Paper
+                || fl.getF2() == Field.Figure.Paper && fl.getF1() == Field.Figure.Rock)
             winner = Win.User;
         else winner = Win.Computer;
     }
 
     public void prntResultsF() {
-        System.out.println("Your move: " + userMoveF + "\nComputer move: " +
-                randNumF);
+        System.out.println("Your move: " + fl.getF2() + "\nComputer move: " +
+                fl.getF1());
         System.out.println("Winner is " + winner);
     }
-    //_________________________________
 
-    /*
-    public void compare () {
-        if (userMove == randNum) winner = Win.Nobody;
-        else if(userMove== 1 && randNum == 2 || userMove == 2 && randNum == 3 || userMove == 3 && randNum == 2)
-             winner = Win.User;
-        else winner = Win.Computer;
-    }*/
-
-    /*public void prntResults() {
-        System.out.println("Your move: " + Field.Options.values()[userMove-1] + "\nComputer move: " +
-                            Field.Options.values()[randNum-1]);
-        System.out.println("Winner is " + winner);
-    }*/
 
     public void start() {
         intro();
